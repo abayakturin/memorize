6d12280d89e8bbdd7f3dc525b3769891b50227b7
Adam Akturin <abayakturin2@gmail.com>
2020-12-24 20:04:31 -0500

    First commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,21 +1,49 @@
 //
-//  ContentView.swift
-//  Memorize
+//  ContentView.swift
+//  Memorize
 //
-//  Created by Adam Akturin on 12/24/20.
+//  Created by Adam Akturin on 12/22/20.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture{
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(viewModel: EmojiMemoryGame())
+        }
     }
 }
