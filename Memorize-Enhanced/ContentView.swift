59727ce405ca93261689c440e6f9aa5a89dd1ba6
Adam Akturin <abayakturin2@gmail.com>
2021-01-25 18:49:22 -0500

    First commit

--- Memorize-Enhanced/ContentView.swift
+++ Memorize-Enhanced/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Memorize Enhanced Edition!")
+        Text("Memorize App Enhanced Edition!")
             .padding()
     }
 }
